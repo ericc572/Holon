24d7e141d7838741a7239350a03c9f64adeb6311
Pranav Shankar <pranav@holon.casa>
2022-01-10 17:50:18 -0500

    changed deploy script, added cfg for requireddeposit, added getter for requireddeposit

--- scripts/deploy-script.js
+++ scripts/deploy-script.js
@@ -1,11 +1,6 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-//IPFS QmfQ3MXNuo9LzPKVn1UFHPw1Tbiz6bKGGPqVrdstNf5rv8
-
 const hre = require("hardhat");
+const { ethers } = require("hardhat");
+const stayManagerConfig = require('../cfg/stay-manager-cfg.json');
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
@@ -14,15 +9,11 @@ async function main() {
   console.log('Deploying contracts with account: ', deployer.address);
   console.log('Account balance: ', accountBalance.toString());
 
-  // const HSD = await hre.ethers.getContractFactory("HolonSecDeposit");
-  // const hsd = await HSD.deploy();
-  // await hsd.deployed();
-  // console.log("HolonSecDeposit deployed to:", hsd.address);
-
-  const OD = await hre.ethers.getContractFactory("OwnerDeposit");
-  const od = await OD.deploy();
-  await od.deployed();
-  console.log("OwnerDeposit deployed to:", od.address);
+  requiredDeposit = ethers.utils.parseUnits(stayManagerConfig['requiredDeposit'], 18);
+  const stayManagerFactory = await hre.ethers.getContractFactory("StayManager");
+  const stayManager = await stayManagerFactory.deploy(requiredDeposit);
+  await stayManager.deployed();
+  console.log("StayManager deployed to:", stayManager.address);
 }
 
 main().then(() => process.exit(0)).catch(error => {
